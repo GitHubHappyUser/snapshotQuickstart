d3ac257378f26d62e8bc786f1da0e3af8105dc2f
GitHubHappyUser <84545480+GitHubHappyUser@users.noreply.github.com>
2021-09-24 10:28:09 +0300

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,28 @@
+package com.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        System.out.println("Application Test 1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        System.out.println("Application Test 2");
+    }
+
+
+}
---
+++ IntegrationTest.java
@@ -0,0 +1,26 @@
+package com.geekbrains;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class IntegrationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        System.out.println("Integration Test 1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        System.out.println("Integration Test 2");
+    }
+}
